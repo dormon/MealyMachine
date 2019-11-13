ae46d27b36b83b0387fb026e99e1dcc2d0d0f344
Tomáš Milet <amillhouse@seznam.cz>
2019-11-13 22:26:53 +0100

    Fixed exceptions now showing a message

--- src/MealyMachine/Exception.h
+++ src/MealyMachine/Exception.h
@@ -6,21 +6,19 @@
 
 class mealyMachine::ex::Exception : public std::runtime_error {
  public:
-  Exception(std::string const& msg) : std::runtime_error(msg) {}
+  Exception(std::string const& msg) : std::runtime_error(msg) {
+    message = std::string("MealyMachine - ") + msg;
+  }
   virtual ~Exception() throw() {}
   virtual char const* what() const throw() override {
-    return std::string(std::string("MealyMachine - ") +
-                       std::runtime_error::what())
-        .c_str();
+    return message.c_str();
   }
+  std::string message;
 };
 
 class mealyMachine::ex::ParsingError : public Exception {
  public:
-  ParsingError(std::string const& msg) : Exception(msg) {}
-  virtual char const* what() const throw() override {
-    return std::string(std::string("MealyMachine::parsing - ") +
-                       std::runtime_error::what())
-        .c_str();
+  ParsingError(std::string const& msg) : Exception(msg) {
+    message = std::string("MealyMachine::parsing - ") + msg;
   }
 };
